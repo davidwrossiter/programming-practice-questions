8d5455879d2a0775e93fdcd5a78e9f6598114e53
IAmNotThink <alex.r.horwood@gmail.com>
2024-12-11 09:46:08 +0000

    moved answers to new folder and added new (albeit broken) one

---
+++ answers/sportsball calculations.py
@@ -0,0 +1,29 @@
+def  calc(info):
+    output={}
+    average=0
+    length=0
+    highest=0
+    for i in info.keys():
+        total=0
+        for j in info[i]:
+            average+=j
+            total +=j
+            length += 1
+        output[i+" score total"] = total
+    for i in output.keys():
+        if output[i] > highest:
+            highest = [i]
+    output[bestScorer]=highest
+    output[overallPerformance]=round(average/length, 2)
+    return info
+
+stuff={
+    "John Smith":[2, 3, 1],
+    "Sarah Jones":[4, 2, 3],
+    "Mike Brown":[0, 1, 2],
+    "Lisa Chen":[3, 3, 3],
+    "Tom Wilson":[2, 2, 0]
+}
+
+out=calc(stuff)
+print(out)
