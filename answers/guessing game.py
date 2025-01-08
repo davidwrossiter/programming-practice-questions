1f3322d202ff85f61e44ed494931ca2311feb6dc
IAmNotThink <alex.r.horwood@gmail.com>
2025-01-08 09:46:59 +0000

    new file:   answers/guessing game.py

---
+++ answers/guessing game.py
@@ -0,0 +1,32 @@
+def validateguess(answer, guess, mode):
+    positions = {}
+    correct = 0
+    for i in range(len(guess)):
+        if guess[i] == answer[i]:
+            positions[i] = guess[i]
+            correct += 1
+        else:
+            positions[i] = "?"
+    if mode == 0:
+        print(positions)
+    else:
+        print(correct)
+    return(correct)
+import random
+correctglobal = 0
+answerglobal = ""
+try:
+    length = int(input("please select length of password to guess (4 is recomended)\n>>>"))
+except(TypeError):
+    print("please input a number *restart program*")
+if input("please type 'easy' (without quotes) if you want to play on easy mode \n>>>") == "easy":
+    modeglobal = 0
+else:
+    modeglobal = 1
+for i in range(length):
+    answerglobal += str(random.randint(0,9))
+while correctglobal != length:
+    guessglobal = input("please make a guess\n>>>")
+    if len(guessglobal) == length:
+        correctglobal = validateguess(answerglobal, guessglobal, modeglobal)
+print("congratulations")
